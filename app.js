12618a28ee2a094da6d74098d62322628446bf1a
ststar8623 <ststar8623@gmail.com>
2017-04-13 15:52:42 -0700

    add view management

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const add = require('./add.js');
 const subtract = require('./subtract.js');
 const multiply = require ('./multiply.js');
 const divide = require('./divide.js');
+
+class ViewManager {
+	connectEventHandlers(){
+
+	}
+
+	onSubmit(event){
+
+	}
+
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
