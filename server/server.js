1dc0c580dafe179a60e7b39795836553153ecbc6
ststar8623 <ststar8623@gmail.com>
2017-04-13 20:09:14 -0700

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -5,26 +5,27 @@ const url = require('url');
 const server = http.createServer(function(request, response){
 	
 	const urlPath = url.parse(request.url).pathname;
-	const filePath = `./client/${urlPath}`;
+	let filePath = `./client/${urlPath}`;
 
 	fs.stat(filePath, function(err, fileInfo){
-		if(err){
-			response.statusCode = 404;
-			response.end(`Resource not found: "${urlPath}"`);
-		} else {
-			if(fileInfo.isDirectory()){
-				filePath += '/index.html';
-			}
-
-			fs.readFile(filePath, function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.end(`Server error: "${err}"`);
-				} else {
-					response.end(data.toString('utf-8'));
-				}
-			});
+		if(!err && fileInfo.isDirectory()){
+			filePath += '/index.html';
 		}
+		fs.exists(filePath, function(doesExist){
+			if(!doesExist){
+				response.statusCode = 404;
+				response.end(`Resource not found: "${urlPath}"`);
+			} else {
+				fs.readFile(filePath, (err, data)=>{
+					if(err){
+						response.statusCode = 500;
+						response.end(`Server error: "${err}"`);
+					} else {
+						response.end(data.toString('UTF-8'));
+					}
+				});
+			}
+		});
 	});
 });
 
